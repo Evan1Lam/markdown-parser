d2f3f27508cfb6c3db7609b301aed1ea2fc83615
Evan1Lam <103228260+Evan1Lam@users.noreply.github.com>
2022-04-21 18:49:42 -0700

    javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,13 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+//define tester class
 public class MarkdownParseTest {
+    //specifies this is a test
     @Test
+
+    //test name
     public void addition() {
+        //checks if two arguments are equal
          assertEquals(2, 1 + 1);
     }
 }    
